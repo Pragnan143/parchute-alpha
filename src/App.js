e5072a807ab63a5fdcdda30acedd7ed338c63e97
Pragnan143 <pragnanmanam8@gmail.com>
2025-01-17 02:05:41 +0530

    SignUp Component Created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import HomePage from "./Pages/HomePage";
+import SignUp from "./Components/SignUp";
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      {/* <HomePage /> */}
+      <SignUp />
     </div>
   );
 }
