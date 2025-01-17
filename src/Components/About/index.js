f110b2a3cc7ecb32007cd5d8b0ae5598fcaaf6e2
Lanka Srinivasudu <lankasrinivasudu8gmail.com@Lankas-Laptop.local>
2025-01-18 02:02:33 +0530

    Task updates

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -32,14 +32,11 @@ const index = () => {
             <br /> <span className=" text-purple-500">Mission</span>
           </p>
           <p className="text-xl  w-[60%] px-4 text-justify">
-            ontrary to popular belief, Lorem Ipsum is not simply random text. It
-            has roots in a piece of classical Latin literature from 45 BC,
-            making it over 2000 years old. Richard McClintock, a Latin professor
-            at Hampden-Sydney College in Virginia, looked up one of the more
-            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
-            going through the cites of the word in classical literature,
-            discovered the undoubtable source. Lorem Ipsum comes from sections
-            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
+          Empowering minds, one course at a time. At Pursuit Technologies, we are redefining 
+          the way people learn by offering live, interactive courses that make education accessible, 
+          engaging, and impactful. Our mission is clear: to connect passionate educators with eager 
+          learners, creating a community where knowledge thrives. Join us to explore a world of 
+          endless learning possibilities. Pursue your dreams with Pursuit Technologies—where education meets innovation.
           </p>
         </div>
       </div>
--- src/Components/Testimonial/index.js
+++ src/Components/Testimonial/index.js
@@ -1,8 +1,42 @@
 import React from "react";
-import Test from "./WhatsApp Image 2025-01-16 at 02.10.41_8019d8e7.jpg";
+import image1 from "./srinivasudu.jpg";
+import image2 from "./Murari.jpeg";
+import image3 from "./Pragnan.jpeg"
+import image4 from "./Shiv.jpeg"
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
+
+const testimonial=[
+    {
+      id:1,
+      name:'Lanka Srinivasudu',
+      img: image1,
+      review:"A data science enthusiast with expertise in AI-enabled education systems, chess FIDE ranking analysis, and sieve algorithms, dedicated to driving innovation and impactful solutions. Passionate about exploring new technologies to solve complex problems effectively."
+    },
+    
+    {
+      id:2,
+      name:'Bala Murari',
+      img: image2,
+      review:"An innovator in technology, passionate about making education systems more accessible and impactful, committed to solving challenges through transformative solutions. Continuously seeking opportunities to contribute to meaningful advancements in the field."
+    },
+  
+    {
+      id:3,
+      name:'Manam Pragnan',
+      img: image3,
+      review:"An AI enthusiast focused on leveraging technology to create impactful, user-centric solutions, ensuring seamless experiences across diverse and evolving domains. Dedicated to combining creativity and precision in every project undertaken."
+    },
+
+    {
+      id:4,
+      name:'Shiv Bhardwaj',
+      img: image4,
+      review:"An undergraduate skilled in C++ and web development, with expertise in Node.js and React.js, blending technical expertise and creativity to build dynamic applications. Striving to continually enhance skills and deliver meaningful contributions."
+    },
+  ];
 
 const index = () => {
   var settings = {
@@ -14,103 +48,47 @@ const index = () => {
     slidesToShow: 3,
     slidesToScroll: 1,
   };
-  return (
+
+
+
+return (
     <section className="flex-col  w-screen bg-purple-50 p-10 gap-10">
-      <p class="text-4xl p-4 text-purple-600 text-center">Testimonial</p>
+      <p class="text-4xl font-semibold text-purple-500 pt-12">Testimonial</p>
       {/* <div className="flex justify-center items-center"> */}
       <Slider className="" {...settings}>
 
-        <div className="w-[80%] h-[80%] justify-center items-center flex p-9">
+        {
+          testimonial.map((testimonial)=>(<div key={testimonial.id} className="w-[80%] h-[80%] justify-center items-center gap-8 flex p-9">
           <div className="flex-col w-[80%] h-[80%] p-4 border-purple-500 border-2 gap-5 rounded-[10px]">
             <div className="flex flex-col gap-5">
               <div className="flex justify-center items-center gap-5">
                 <div className="w-[120px] h-[100px] float-left rounded-[50%] overflow-hidden">
-                  <img src={Test} className="w-[100%] h-[100%]" />
+                  <img src={testimonial.img} className="w-[100%] h-[100%]" />
                 </div>
                 <div className="w-[100%] h-[40%] justify-center">
                   <div>
-                    <p className="font-semibold text-2xl">Lanka Srinivasudu</p>
+                    <p className="font-semibold text-xl">{testimonial.name}</p>
                     <p className="">Full Stack developer</p>
                   </div>
                 </div>
               </div>
               <div>
-                <p className="text-justify">
-                  I'm a daya secience entushist have done projects on Ai- enabled
-                  personlised education system, chess-fide ranking, sives of
-                  erathones I'm a daya secience entushist have done projects on
-                  Ai- enabled personlised education system, chess-fide ranking,
-                  sives of erathones I'm a daya secience entushist have done
-                  projects on Ai- enabled personlised education system, chess-fide
-                  ranking, sives of erathones
-                </p>
+                <p className="text-justify">{testimonial.review}</p>
               </div>
             </div>
           </div>
-        </div>
+        </div>))
+        }
+        
 
-        <div className="w-[80%] h-[80%] justify-center items-center gap-8 flex p-9">
-          <div className="flex-col w-[80%] h-[80%] p-4 border-purple-500 border-2 gap-5 rounded-[10px]">
-            <div className="flex flex-col gap-5">
-              <div className="flex justify-center items-center gap-5">
-                <div className="w-[120px] h-[100px] float-left rounded-[50%] overflow-hidden">
-                  <img src={Test} className="w-[100%] h-[100%]" />
-                </div>
-                <div className="w-[100%] h-[40%] justify-center">
-                  <div>
-                    <p className="font-semibold text-2xl">Lanka Srinivasudu</p>
-                    <p className="">Full Stack developer</p>
-                  </div>
-                </div>
-              </div>
-              <div>
-                <p className="text-justify">
-                  I'm a daya secience entushist have done projects on Ai- enabled
-                  personlised education system, chess-fide ranking, sives of
-                  erathones I'm a daya secience entushist have done projects on
-                  Ai- enabled personlised education system, chess-fide ranking,
-                  sives of erathones I'm a daya secience entushist have done
-                  projects on Ai- enabled personlised education system, chess-fide
-                  ranking, sives of erathones
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="w-[80%] h-[80%] justify-center items-center gap-8 flex p-9">
-          <div className="flex-col w-[80%] h-[80%] p-4 border-purple-500 border-2 gap-5 rounded-[10px]">
-            <div className="flex flex-col gap-5">
-              <div className="flex justify-center items-center gap-5">
-                <div className="w-[120px] h-[100px] float-left rounded-[50%] overflow-hidden">
-                  <img src={Test} className="w-[100%] h-[100%]" />
-                </div>
-                <div className="w-[100%] h-[40%] justify-center">
-                  <div>
-                    <p className="font-semibold text-2xl">Lanka Srinivasudu</p>
-                    <p className="">Full Stack developer</p>
-                  </div>
-                </div>
-              </div>
-              <div>
-                <p className="text-justify">
-                  I'm a daya secience entushist have done projects on Ai- enabled
-                  personlised education system, chess-fide ranking, sives of
-                  erathones I'm a daya secience entushist have done projects on
-                  Ai- enabled personlised education system, chess-fide ranking,
-                  sives of erathones I'm a daya secience entushist have done
-                  projects on Ai- enabled personlised education system, chess-fide
-                  ranking, sives of erathones
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+        
 
       </Slider>
       {/* </div> */}
+
     </section>
   );
 };
+
 
 export default index;
